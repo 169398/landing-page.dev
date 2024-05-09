c138242297fab565cebd4c87e57ad881f8a71077
169398 <kulubiidris@gmail.com>
2024-05-09 15:58:39 +0300

    company values

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -9,39 +9,25 @@ export function CardUi() {
 }
 export const projects = [
   {
-    title: "Stripe",
+    title: "Effortless Setup",
     description:
-      "A technology company that builds economic infrastructure for the internet.",
-    link: "https://stripe.com",
+      "Sign in,create your shop ,add pictures and display your products in minutes  .",
+    
   },
   {
-    title: "Netflix",
+    title: "Massive Audience",
     description:
-      "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
-    link: "https://netflix.com",
+      "Connect with thousands of buyers close to you and from different cumpuses for free.",
   },
   {
-    title: "Google",
+    title: "Safe Transaction",
     description:
-      "A multinational technology company that specializes in Internet-related services and products.",
-    link: "https://google.com",
+      "Swipe handles all our payments  so you focus on selling and customers to buying.",
   },
   {
-    title: "Meta",
+    title: "Fast Communications ",
     description:
-      "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
-    link: "https://meta.com",
+      "Inbuilt chat allows instant communication with customers for efficient business.",
   },
-  {
-    title: "Amazon",
-    description:
-      "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
-    link: "https://amazon.com",
-  },
-  {
-    title: "Microsoft",
-    description:
-      "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
-    link: "https://microsoft.com",
-  },
+ 
 ];
